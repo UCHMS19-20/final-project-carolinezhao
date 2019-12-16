95508ff37c8a71e082934b654336c129adced0a4
carolinezhao20 <57998984+carolinezhao20@users.noreply.github.com>
2019-12-16 10:48:57 -0500

    Update game.py
    Adding more to the pre-trail introduction.

--- src/game.py
+++ src/game.py
@@ -5,7 +5,7 @@ import random
 health = 100
 hunger = 0
 disease = 0
-time = 0
+days = 0
 distance_remaining = 2000
 food = 100
 wheels = 4
@@ -13,13 +13,21 @@ oxen = 4
 clothes = 5
 
 print("Welcome to the Oregon Trail.")
+time.sleep(1)
 name = ""
 while name == "":
-    name = input("What is your name?")
+    name = input("What is your name? ")
     for n in name.lower():
         if n not in "abcdefghijklmnopqrstuvwxyz":
             print("That's not valid, try again.")
             name = ""
             break
 print(f"Hello {name.title()}.")
-            
+time.sleep(1)
+print("Right now, we are in Independence, Missouri.")
+time.sleep(1)
+print("You want to go out west, and the Oregon Trail is the best way to do so.")
+time.sleep(1)
+print("Today is April 1st. Your goal is to make it to Oregon before December 15, when winter will fall.")
+time.sleep(1)
+print("It's a dangerous journey, but I have faith in you. Good luck!")
