9231f208d04ff9a6db076b50bbbcdde13dedc12b
carolinezhao20 <57998984+carolinezhao20@users.noreply.github.com>
2020-01-10 17:49:03 -0500

    basic structure of game (minus graphics)

--- src/game.py
+++ src/game.py
@@ -6,6 +6,7 @@ health = 100
 typhoid = False
 cholera = False
 dysentery = False
+measles = False
 days = 0
 distance = 2000
 food = 500
@@ -41,11 +42,11 @@ def intro():
 def status():
   print("Your current status is as follows:")
   if health >= 75:
-    print("Health: Good")
+    print(f"Health: Good ({health})")
   elif 75 > health >= 50:
-    print("Health: Fair")
+    print(f"Health: Fair ({health})")
   else:
-    print("Health: Poor")
+    print(f"Health: Poor ({health})")
   if diseases == []:
     print("Disease(s) Present: None")
   else:
@@ -60,7 +61,7 @@ def status():
   print(f"Distance Remaining: {distance} miles")
   return
 
-def choose_an_option(days):
+def choose_an_option():
   print("\n")
   time.sleep(1)
   print(f"This is day {1+days} of your trip.")
@@ -102,31 +103,60 @@ def travel():
       elif 75 > health >= 50:
         distance_travelled = random.randint(7, 13)
       else:
-            distance_travelled = random.randint(5, 7)
+        distance_travelled = random.randint(5, 7)
   if what_to_judge_travel_by == 2:
       if oxen > 4:
         distance_travelled = random.randint(14, 20)
       elif 4 >= oxen > 2:
         distance_travelled = random.randint(7, 14)
-      elif oxen =< 0:
+      elif oxen == 0:
         distance_travelled = 1
       else:
-            distance_travelled = random.randint(5, 7)
+        distance_travelled = random.randint(5, 7)
   if what_to_judge_travel_by == 3:
       if wheels >=4:
         distance_travelled = random.randint(12, 18)
-      elif wheels =< 0:
+      elif wheels == 0:
         distance_travelled = 1
       else:
-            distance_travelled = random.randint(5, 11)
+        distance_travelled = random.randint(5, 11)
   print(f"You travelled {distance_travelled} miles.")
   return(distance_travelled)
 
 def rest():
+  global typhoid
+  global cholera
+  global dysentery
+  global measles
+  global diseases
   possible_health_gains = [5, 5, 5, 10, 10, 10, 10, 15, 15, 20]
   index = random.randint(0,9)
   health_gain = possible_health_gains[index]
   print(f"You gained {health_gain} in health.")
+
+  if diseases != []:
+    recover_disease = random.randint(1,4)
+    if recover_disease == 2:
+      if typhoid == True:
+        print("You recovered from typhoid.")
+        typhoid = False
+        diseases.remove("typhoid")
+        health_gain += 25
+      elif measles == True:
+        print("You recovered from the measles.")
+        measles = False
+        diseases.remove("measles")
+        health_gain += 25
+      elif dysentery == True:
+        print("You recovered from dysentery.")
+        dysentery = False
+        diseases.remove("dysentery")
+        health_gain += 25
+      elif cholera == True:
+        print("You recovered from cholera.")
+        cholera = False
+        diseases.remove("cholera")
+        health_gain += 25
   return(health_gain)
 
 def buy():
@@ -135,50 +165,228 @@ def buy():
   global oxen
   global clothes
   purchase = random.randint(1, 8)
-  if purchase == 1:
+  if purchase == 1 and money >= 20:
     oxen += 1
     money_spent = 20
     print("You spent $20 on 1 ox.")
-  if purchase == 2:
+  elif purchase == 2 and money >= 10:
     clothes += 1
     money_spent = 10
     print("You spent $10 on 1 set of clothing.")
-  if purchase == 3:
+  elif purchase == 3 and money >= 10:
     wheels += 1
     money_spent = 10
     print("You spent $10 on 1 wheel.")
-  if purchase == 4:
+  elif purchase == 4 and money >= 20:
     food += 100
     money_spent = 20
     print("You spent $20 on 100 lbs of food.")
-  if purchase == 5:
+  elif purchase == 5 and money >= 40:
     oxen += 2
     money_spent = 40
     print("You spent $40 on 2 oxen.")
-  if purchase == 6:
+  elif purchase == 6 and money >= 20:
     clothes += 2
     money_spent = 20
     print("You spent $20 on 2 sets of clothing.")
-  if purchase == 7:
+  elif purchase == 7 and money >= 20:
     wheels += 2
     money_spent = 20
     print("You spent $20 on 2 wheels.")
-  if purchase == 8:
+  elif purchase == 8 and money >= 40:
     food += 200
     money_spent = 40
     print("You spent $40 on 200 lbs of food.")
+  else:
+    print("You don't have enough money to make a purchase today. What a waste of a day.")
+    money_spent = 0
   return(money_spent)
 
 def hunt():
-  possible_food_gains = [0, 20, 40, 60, 80, 100]
-  index = random.randint(0,5)
+  possible_food_gains = [80, 0, 80, 90, 20, 100, 40, 60, 80, 100, 100]
+  index = random.randint(0,10)
   food_gain = possible_food_gains[index]
   print(f"You gained {food_gain} lbs of food.")
   return(food_gain)
 
+def eat():
+  if health >= 75:
+    food_eaten = random.randint(13, 17)
+  elif 75 > health >= 50:
+    food_eaten = random.randint(15, 20)
+  else:
+    food_eaten = random.randint(17, 25)
+  if food < food_eaten:
+    food_eaten = food
+  print(f"During the day, you ate {food_eaten} pounds of food.")
+  return(food_eaten)
+
+def sick():
+  global typhoid
+  global cholera
+  global dysentery
+  global measles
+  global diseases
+  global health
+  get_disease = random.randint(0,10)
+  if get_disease == 1 or get_disease == 5:
+    disease_gotten = random.randint(1,4)
+    if disease_gotten == 1 and typhoid == False:
+      print("You have fallen ill with typhoid.")
+      typhoid = True
+      diseases.append("typhoid")
+      health -= 30
+    elif disease_gotten == 2 and cholera == False:
+      print("You have fallen ill with cholera.")
+      cholera = True
+      diseases.append("cholera")
+      health -= 30
+    elif disease_gotten == 3 and dysentery == False:
+      print("You have fallen ill with dysentery.")
+      dysentery = True
+      diseases.append("dysentery")
+      health -= 30
+    elif disease_gotten == 4 and measles == False:
+      print("You have fallen ill with the measles.")
+      measles = True
+      diseases.append("measles")
+      health -= 30
+  return
+
+def thief():
+  global food
+  global wheels
+  global oxen
+  global clothes
+  global money
+  chance_of_thief = random.randint(0,10)
+  if chance_of_thief == 6 or chance_of_thief == 9:
+    item_stolen = random.randint(1,5)
+    amount_stolen = random.randint(1,4)
+    if item_stolen == 1 and food > 0:
+      if amount_stolen == 1 and food > 300:
+        print("Thieves have come at night to steal 300 lbs of food.")
+        food -= 300
+      elif amount_stolen == 2 and food > 200:
+        print("Thieves have come at night to steal 200 lbs of food.")
+        food -= 200
+      elif amount_stolen == 3 and food > 100:
+        print("Thieves have come at night to steal 100 lbs of food.")
+        food -= 100
+      elif amount_stolen == 4 and food > 50:
+        print("Thieves have come at night to steal 50 lbs of food.")
+        food -= 50
+      else:
+        print("Thieves have come at night to steal all of your food.")
+        food = 0
+    if item_stolen == 2 and wheels > 0:
+      if amount_stolen == 1 and wheels > 1:
+        print("Thieves have come at night to steal 1 wheel.")
+        wheels -= 1
+      elif amount_stolen == 2 and wheels > 2:
+        print("Thieves have come at night to steal 2 wheels.")
+        wheels -= 2
+      elif amount_stolen == 1 and wheels > 1:
+        print("Thieves have come at night to steal 1 wheel.")
+        wheels -= 1
+      elif amount_stolen == 4 and wheels > 2:
+        print("Thieves have come at night to steal 2 wheels.")
+        wheels -= 2
+      else:
+        print("Thieves have come at night to steel all of your wheels.")
+        wheels = 0
+    if item_stolen == 3 and oxen > 0:
+      if amount_stolen == 1 and oxen > 1:
+        print("Thieves have come at night to steal 1 ox.")
+        oxen -= 1
+      elif amount_stolen == 2 and oxen > 2:
+        print("Thieves have come at night to steal 2 oxen.")
+        oxen -= 2
+      elif amount_stolen == 3 and oxen >3:
+        print("Thieves have come at night to steal 3 oxen.")
+        oxen -=3
+      elif amount_stolen == 4 and oxen > 2:
+        print("Thieves have come at night to steal 2 oxen.")
+        oxen -= 2
+      else:
+        print("Thieves have come at night to steel all of your oxen.")
+        oxen = 0
+    if item_stolen == 4 and clothes > 0:
+      if amount_stolen == 1 and clothes >2:
+        print("Thieves have come at night to steal 2 sets of clothing.")
+        clothes -= 2
+      elif amount_stolen == 2 and clothes > 1:
+        print("Thieves have come at night to steal 1 set of clothing.")
+        clothes -= 1
+      elif amount_stolen == 3 and clothes > 1:
+        print("Thieves have come at night to steal 1 set of clothing.")
+        clothes -= 1
+      elif amount_stolen == 4 and clothes > 2:
+        print("Thieves have come at night to steal 2 sets of clothing.")
+        clothes -= 2
+      else:
+        print("Thieves have come at night to steal all of your clothing.")
+        clothes = 0
+    if item_stolen == 5 and money > 0:
+      if amount_stolen == 1 and money > 100:
+        print("Thieves have come at night to steal $100.")
+        money -= 100
+      elif amount_stolen == 2 and money > 200:
+        print("Thieves have come at night to steal $200.")
+        money -= 200
+      elif amount_stolen == 3 and money > 300:
+        print("Thieves have come at night to steal $300.")
+        money -= 300
+      elif amount_stolen == 4 and money > 50:
+        print("Thieves have come at night to steal $50.")
+        money -= 50
+      else:
+        print("Thieves have come at night to steal all of your money.")
+  return
+
+def reevaluate_health():
+  health_decrease = 0
+
+  if len(diseases) == 1:
+    health_decrease += 2
+  elif len(diseases) == 2:
+    health_decrease += 3
+  elif len(diseases) == 3:
+    health_decrease += 5
+  elif len(diseases) == 4:
+    health_decrease += 8
+  
+  if food == 0:
+    health_decrease += 15
+  elif food < 50:
+    health_decrease += 5
+  elif food < 100:
+    health_decrease += 3
+  elif food < 150:
+    health_decrease += 1
+  
+  if clothes == 0:
+    health_decrease += 10
+  elif clothes < 3:
+    health_decrease += 2
+  
+  if health < health_decrease:
+    health_decrease = health
+  return(health_decrease)
+
+def continue_game():
+  if health <= 0:
+    return False
+  if days == 259:
+    return False
+  if distance == 0:
+    return False
+  else:
+    return True
+
 intro()
 while game == True:
-  option = choose_an_option(days)
+  option = choose_an_option()
   if option == "1":
     distance -= travel()
   elif option == "2":
@@ -187,4 +395,9 @@ while game == True:
     money -= buy()
   elif option == "4":
     food += hunt()
+  health -= reevaluate_health()
+  food -= eat()
+  sick()
+  thief()
   days += 1
+  game = continue_game()
